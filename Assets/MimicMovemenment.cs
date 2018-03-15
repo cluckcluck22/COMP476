b563210f7a6bba1e789a6e2bdc30da4736fcf814
Sabz <sabrina.d.mello@hotmail.com>
2018-03-15 12:10:11 -0400

    Added the animation of Eating, Attack, Rest, Talk to the mimic controller

--- Assets/MimicMovemenment.cs
+++ Assets/MimicMovemenment.cs
@@ -8,12 +8,15 @@ public class MimicMovemenment : MonoBehaviour
     // Movement Attributes
     private float horizontal;
     private float vertical;
+    public float m_speed;
     public float m_WalkSpeed = 5.0f;
     public float m_RunSpeed = 8.0f;
     public float mAngularSpeed = 180.0f;
-    
+
     //Animations
     private AnimatorDriverAnimal m_AnimatorDriverAnimal;
+    private bool isWalking;
+    private bool isRunning;
 
     void Start()
     {
@@ -34,42 +37,98 @@ public class MimicMovemenment : MonoBehaviour
         return (horizontal != 0) || (vertical != 0);
     }
 
+    private void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            m_speed = m_RunSpeed;   //Running
+            isRunning = true;
+            isWalking = false;
+        }
+        else
+        {
+            m_speed = m_WalkSpeed;  //Walking
+            isWalking = true;
+            isRunning = false;
+        }
+
+        Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+        // Cap the magnitude of direction vector
+        direction = Vector3.ClampMagnitude(direction, 1.0f);
+        // Translate the game object in world space
+        transform.Translate(direction * m_speed * Time.deltaTime, Space.World);
+        // Rotate the game object
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), mAngularSpeed * Time.deltaTime);
+
+        //Animate
+        if (isWalking)
+        {
+            m_AnimatorDriverAnimal.PlayWalk(); 
+        }
+        if (isRunning)
+        { 
+            m_AnimatorDriverAnimal.PlayRun();
+        }
+    }
+
     private void Eating()
     {
-            m_AnimatorDriverAnimal.PlayFullBodyState(States.AnimalFullBody.Eat);
+        m_AnimatorDriverAnimal.PlayFullBodyState(States.AnimalFullBody.Eat);
     }
 
-    private bool Attack()
+    private void Attack()
     {
-        return false;
+        m_AnimatorDriverAnimal.PlayLayeredState(States.AnimalLayered.Attack);
     }
 
+    private void Rest()
+    {
+        m_AnimatorDriverAnimal.PlayFullBodyState(States.AnimalFullBody.Rest);
+    }
+
+    private void Talk()
+    {
+        m_AnimatorDriverAnimal.PlayLayeredState(States.AnimalLayered.Talk);
+    }
     private void InputHandler()
     {
         if (isMoving())
         {
-                /**WALKING
-                    The code below is only walking at the moment
-                */
-                Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
-                // Cap the magnitude of direction vector
-                direction = Vector3.ClampMagnitude(direction, 1.0f);
-                // Translate the game object in world space
-                transform.Translate(direction * m_WalkSpeed * Time.deltaTime, Space.World);
-                // Rotate the game object
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), mAngularSpeed * Time.deltaTime);
-                //Animate
-                m_AnimatorDriverAnimal.PlayWalk();
+            Movement();
         }
-        else if(!isMoving())
+        else if (!isMoving())
         {
+            //Not moving idle
             m_AnimatorDriverAnimal.PlayFullBodyState(States.AnimalFullBody.Idle);
         }
 
+        //Eating
+        /**NOTE: There is a delay in the eating...you have to press E twice after Moving for it to eat.
+        * Possibly a problem with animation blending
+        */
         if (Input.GetKeyDown(KeyCode.E))
         {
             Eating();
         }
+        
+        //Attack
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Attack();
+        }
+
+        //Sleep
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Rest();
+        }
+
+        //Sleep
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Talk();
+        }
+
     }
 
     //SPEEED conditions, keeping track of the speed of the mimic, once it reaches a certain speed we do either playWalk(), playRun(), Idle
