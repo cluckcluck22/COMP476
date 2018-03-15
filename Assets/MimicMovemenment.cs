710bbb6b9bc02dd225d7c773456c0fe9ce7907ab
Sabz <sabrina.d.mello@hotmail.com>
2018-03-15 11:29:22 -0400

    Making an input-handler

--- Assets/MimicMovemenment.cs
+++ Assets/MimicMovemenment.cs
@@ -2,68 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MimicMovemenment : MonoBehaviour {
-    // Units per second
-    [SerializeField]
-    float mSpeed = 5.0f;
+public class MimicMovemenment : MonoBehaviour
+{
 
-    // Degrees per second
-    [SerializeField]
-    float mAngularSpeed = 180.0f;
-
-    // Idle timer variables
-    [SerializeField]
-    float mIdleTime = 2.0f;
-    float mTimer = 0.0f;
-
-    Vector3 mDefaultScale;
-
+    // Movement Attributes
+    private float horizontal;
+    private float vertical;
+    public float m_WalkSpeed = 5.0f;
+    public float m_RunSpeed = 8.0f;
+    public float mAngularSpeed = 180.0f;
+    
+    //Animations
     private AnimatorDriverAnimal m_AnimatorDriverAnimal;
 
     void Start()
     {
         // Keep a backup of the original scale
         m_AnimatorDriverAnimal = GetComponent<AnimatorDriverAnimal>();
-        mDefaultScale = transform.localScale;
-     
     }
 
     void Update()
     {
-       if(!Moving()) 
-            m_AnimatorDriverAnimal.PlayFullBodyState(States.AnimalFullBody.Idle);
-       
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
+        InputHandler();
     }
-    private bool Moving()
+
+    private bool isMoving()
     {
-        // Obtain input information (See "Horizontal" and "Vertical" in the Input Manager)
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        return (horizontal != 0) || (vertical != 0);
+    }
 
-        // Check if there is movement
-        if (!Mathf.Approximately(vertical, 0.0f) || !Mathf.Approximately(horizontal, 0.0f))
-        {
-            Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+    private void Eating()
+    {
+            m_AnimatorDriverAnimal.PlayFullBodyState(States.AnimalFullBody.Eat);
+    }
 
-            // Cap the magnitude of direction vector
-            direction = Vector3.ClampMagnitude(direction, 1.0f);
-
-            // Translate the game object in world space
-            transform.Translate(direction * mSpeed * Time.deltaTime, Space.World);
-
-            // Rotate the game object
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), mAngularSpeed * Time.deltaTime);
-
-            m_AnimatorDriverAnimal.PlayWalk();
-
-            // Reset idle timer to zero
-            mTimer = 0.0f;
-            transform.localScale = mDefaultScale;
-
-            return true;
-        }
+    private bool Attack()
+    {
         return false;
     }
 
+    private void InputHandler()
+    {
+        if (isMoving())
+        {
+                /**WALKING
+                    The code below is only walking at the moment
+                */
+                Vector3 direction = new Vector3(horizontal, 0.0f, vertical);
+                // Cap the magnitude of direction vector
+                direction = Vector3.ClampMagnitude(direction, 1.0f);
+                // Translate the game object in world space
+                transform.Translate(direction * m_WalkSpeed * Time.deltaTime, Space.World);
+                // Rotate the game object
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), mAngularSpeed * Time.deltaTime);
+                //Animate
+                m_AnimatorDriverAnimal.PlayWalk();
+        }
+        else if(!isMoving())
+        {
+            m_AnimatorDriverAnimal.PlayFullBodyState(States.AnimalFullBody.Idle);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Eating();
+        }
+    }
+
+    //SPEEED conditions, keeping track of the speed of the mimic, once it reaches a certain speed we do either playWalk(), playRun(), Idle
+
+    //Transforming the mimic: mimic management. DestoryObject(),
+    //Camera,destory anything below the camera
+    //onAwake the character and attach to the camera, and when destoried detach the camera
+    //Empty gameObject.
+    //swapping transform - copy Bessie & real Bessie (storing the states  information)
 }
