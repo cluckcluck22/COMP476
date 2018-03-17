d9070f26ba478b5d0dba7d98ad62c3cb84c689ea
Jennifer Sunahara <jennifer.sunahara@gmail.com>
2018-03-17 00:46:44 -0400

    Rough NavMesh and Agent

---
+++ Assets/Scripts/AI/GoTo.cs
@@ -0,0 +1,13 @@
+﻿// MoveTo.cs
+    using UnityEngine;
+    using System.Collections;
+    
+    public class GoTo : MonoBehaviour {
+       
+       public Transform goal;
+       
+       void Start () {
+          UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+          agent.destination = goal.position; 
+       }
+    }
