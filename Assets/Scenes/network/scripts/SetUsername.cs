01bf0e5b6d61ae1f8a776f7b17b570cd2f83f7e2
Eric Davies <itsericrogers.com@hotmail.com>
2018-03-20 16:43:39 -0400

    Intro Scene to launch the game from

---
+++ Assets/Scenes/network/scripts/SetUsername.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetUsername : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void setUsername(GameObject text)
+    {
+        NetworkInformation.username = text.GetComponent<InputField>().text;
+    }
+}
---
+++ Assets/Scenes/network/scripts/getUsername.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class getUsername : MonoBehaviour {
+
+    Text mText;
+
+    // Use this for initialization
+    void Start()
+    {
+        mText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mText.text = "Current Username: " + NetworkInformation.username;
+    }
+}
