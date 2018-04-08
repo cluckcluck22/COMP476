b25cb691d42f8fbe524c8ebcc25972d4b554fdd3
Jennifer Sunahara <jennifer.sunahara@gmail.com>
2018-04-08 00:57:36 -0400

    BaseConfig with properties
    Hen, Ram and Cow prefabs have configs now

--- Assets/Data/AnimalConfig.cs
+++ Assets/Data/AnimalConfig.cs
@@ -3,16 +3,17 @@
 [CreateAssetMenu(fileName = "AnimalConfig")]
 public class AnimalConfig : ScriptableObject
 {
-
     public Species species;
-    public AnimalConfig baseAnimalConfig; // placehodler type
+    public BaseAnimalConfig baseAnimalConfig; // placehodler type
 
-    public float randomMinRatio;    // move this
-    public float randomMaxRatio;    // move this
-
-    public AnimationCurve hungerNeedCurve
+    public float randomMinRatio
     {
-        get { return baseAnimalConfig.m_hungerNeedCurve; }
+        get { return baseAnimalConfig.m_randomMinRatio; }
+    }   
+
+    public float randomMaxRatio
+    {
+        get { return baseAnimalConfig.m_randomMaxRatio; }
     }
 
     public int maxHunger;
@@ -20,35 +21,68 @@ public class AnimalConfig : ScriptableObject
     public int hungerRecuperation;
     [Tooltip("Amount loss per second")]
     public int hungerLossRate;
-    [Tooltip("Normalized need score wrt current value")]
-    public AnimationCurve m_hungerNeedCurve;    // move this
+
+    public AnimationCurve hungerNeedCurve
+    {
+        get { return baseAnimalConfig.m_hungerNeedCurve; }
+    }
 
     public int maxFatigue;
     [Tooltip("Amount recovered per second while resting")]
     public int fatigueRecuperation;
     [Tooltip("Amount loss per second")]
     public int fatigueLossRate;
-    [Tooltip("Normalized need score wrt current value")]
-    public AnimationCurve fatigueNeedCurve;     // move this
+
+    public AnimationCurve fatigueNeedCurve
+    {
+        get { return baseAnimalConfig.m_fatigueNeedCurve; }
+    }
 
     public int maxBoredom;
     [Tooltip("Amount recovered per second while playing")]
     public int boredomRecuperation;
     [Tooltip("Amount loss per second")]
     public int boredomLossRate;
-    [Tooltip("Normalized need score wrt current value")]
-    public AnimationCurve boredomNeedCurve;     // move this
 
-    [Tooltip("Normalized need score wrt avg distance from friends")]
-    public AnimationCurve affinityProximityNeedCurve;   // move this
+    public AnimationCurve boredomNeedCurve
+    {
+        get { return baseAnimalConfig.m_boredomNeedCurve; }
+    }
+
+    public AnimationCurve affinityProximityNeedCurve
+    {
+        get { return baseAnimalConfig.m_affinityProximityNeedCurve; }
+    }
 
     public int maxHealth;
 
-    public float idleScore;             // move this
+    public float idleScore
+    {
+        get { return baseAnimalConfig.m_idleScore; }
+    }
 
-    public float fov;                   // move this
-    public float sightDistance;         // move this
-    public float lkpSightDistance;      // move this
-    public Vector3 headOffset;          // move this
-    public float lkpExpire;             // move this
+    public float fov
+    {
+        get { return baseAnimalConfig.m_fov; }
+    }
+
+    public float sightDistance
+    {
+        get { return baseAnimalConfig.m_sightDistance; }
+    }   
+    
+    public float lkpSightDistance
+    {
+        get { return baseAnimalConfig.m_lkpSightDistance; }
+    }    
+    
+    public Vector3 headOffset
+    {
+        get { return baseAnimalConfig.m_headOffset; }
+    }    
+    
+    public float lkpExpire
+    {
+        get { return baseAnimalConfig.m_lkpExpire; }
+    }            
 }
---
+++ Assets/Data/BaseAnimalConfig.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BaseAnimalConfig")]
+public class BaseAnimalConfig : ScriptableObject
+{
+    public float m_randomMinRatio;    
+    public float m_randomMaxRatio;    
+                                                      
+    [Tooltip("Normalized need score wrt current value")]
+    public AnimationCurve m_hungerNeedCurve;    
+    [Tooltip("Normalized need score wrt current value")]
+    public AnimationCurve m_fatigueNeedCurve;
+    [Tooltip("Normalized need score wrt current value")]
+    public AnimationCurve m_boredomNeedCurve;
+    [Tooltip("Normalized need score wrt avg distance from friends")]
+    public AnimationCurve m_affinityProximityNeedCurve;
+
+
+    public float m_idleScore;             
+
+    public float m_fov;                   
+    public float m_sightDistance;         
+    public float m_lkpSightDistance;      
+    public Vector3 m_headOffset;          
+    public float m_lkpExpire;             
+
+}
