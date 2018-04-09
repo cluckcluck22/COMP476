629de872afe270333d36964a9edfd75af5b241db
Sabz <sabrina.d.mello@hotmail.com>
2018-04-09 17:43:37 -0400

    use F1, F2, F3, F to open animal select. E and Q to select a animal. (currently nothing happens when selected)

--- Assets/Scripts/AI_Panel/AnimalPicker.cs
+++ Assets/Scripts/AI_Panel/AnimalPicker.cs
@@ -1,19 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AnimalPicker : MonoBehaviour {
+public class AnimalPicker : MonoBehaviour
+{
 
     bool SelectionEnabled;
     GameObject currentChoice;
     bool F1, F2, F3, F4;
+    int counter = 0;
+    GameObject[] animal_arr;
 
     //Toggle Options
     public GameObject CowChoices;
     public GameObject PigChoices;
     public GameObject SheepChoices;
     public GameObject RamChoices;
-    
+
     //Actual transforms of the Animal (clones)
     public GameObject[] MimicAnimalOptions;
     public Transform[] cows;
@@ -22,13 +26,13 @@ public class AnimalPicker : MonoBehaviour {
     public Transform[] rams;
     bool haveClones = false;
 
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start()
     {
     }
-	
-	// Update is called once per frame
-	void Update () 
+
+    // Update is called once per frame
+    void Update()
     {
         //if(!haveClones) {
         //    MimicAnimalOptions = GameObject.FindGameObjectsWithTag("AI_Clones"); //Getting animals but we want clones
@@ -45,39 +49,57 @@ public class AnimalPicker : MonoBehaviour {
     //Display the appropriate list of animal sub-category for player to see.
     void HandleInput()
     {
-        if(Input.GetKeyDown(KeyCode.F1) && !F2 && !F3 && !F4)
-        {       
+
+        if (Input.GetKeyDown(KeyCode.F1) && !F2 && !F3 && !F4)
+        {
             currentChoice = CowChoices;
+            animal_arr = new GameObject[currentChoice.transform.childCount];
             SelectionEnabled = !SelectionEnabled;
-            F1 = !F1;       
+            F1 = !F1;
         }
-        else if(Input.GetKeyDown(KeyCode.F2) && !F1 && !F3 && !F4) 
+        else if (Input.GetKeyDown(KeyCode.F2) && !F1 && !F3 && !F4)
         {
             currentChoice = PigChoices;
+            animal_arr = new GameObject[currentChoice.transform.childCount];
             SelectionEnabled = !SelectionEnabled;
             F2 = !F2;
         }
-        else if(Input.GetKeyDown(KeyCode.F3) && !F1 && !F2 && !F4) 
+        else if (Input.GetKeyDown(KeyCode.F3) && !F1 && !F2 && !F4)
         {
             currentChoice = SheepChoices;
+            animal_arr = new GameObject[currentChoice.transform.childCount];
             SelectionEnabled = !SelectionEnabled;
             F3 = !F3;
         }
-        else if(Input.GetKeyDown(KeyCode.F4) && !F1 && !F2 && !F3) 
+        else if (Input.GetKeyDown(KeyCode.F4) && !F1 && !F2 && !F3)
         {
             currentChoice = RamChoices;
+            animal_arr = new GameObject[currentChoice.transform.childCount];
             SelectionEnabled = !SelectionEnabled;
             F4 = !F4;
         }
-        
+
 
         if (SelectionEnabled)
-            currentChoice.SetActive(true);
+        { 
+            currentChoice.SetActive(true); 
+        }
         else
             currentChoice.SetActive(false);
+
+
+        if (Input.GetKeyDown(KeyCode.E) && SelectionEnabled)
+        {
+            ChooseSelectionForward(currentChoice);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && SelectionEnabled)
+        {
+            ChooseSelectionBackward(currentChoice);
+        }
     }
 
-    void setFilteredList() 
+    void setFilteredList()
     {
         cows = GetFilteredAnimalType("_Cow");
         pigs = GetFilteredAnimalType("_Pig");
@@ -86,20 +108,49 @@ public class AnimalPicker : MonoBehaviour {
     }
 
     //returns a filtered list of animal clones of designated (type)
-    Transform[] GetFilteredAnimalType(string type) {
+    Transform[] GetFilteredAnimalType(string type)
+    {
         //TODO
         return null;
     }
 
-    void ChooseSelection(GameObject[] animal)
+    void ChooseSelectionForward(GameObject currentChoice)
     {
-        int counter = 0;
-        animal = new GameObject[transform.childCount];
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            animal[i] = transform.GetChild(i).gameObject;
-        }
+        Debug.Log("In choose seclection");
+
+        PopulateTheAnimalArr(currentChoice);
+
+        //Move through the array and when on the selected choice highlight the selection:
+        animal_arr[counter % animal_arr.Length].GetComponent<Toggle>().isOn = true;
+        counter++;
+
+        //Create another function that will add the  transform of the clone set to a certain number....1, 2, 3, 4
     }
 
+    void ChooseSelectionBackward(GameObject currentChoice)
+    {
+        if (counter == 0) 
+            counter = animal_arr.Length;
+
+        
+        PopulateTheAnimalArr(currentChoice);
+
+        //Move through the array and when on the selected choice highlight the selection:
+        animal_arr[counter % animal_arr.Length].GetComponent<Toggle>().isOn = true;
+        counter--;
+    }
+
+    void PopulateTheAnimalArr(GameObject currentChoice)
+    {
+        //Populated the array with the toggle choices for that animal
+        for (int i = 0; i < animal_arr.Length; i++)
+        {
+            animal_arr[i] = currentChoice.transform.GetChild(i).gameObject;
+            if (i == 0)
+            {
+                animal_arr[i].GetComponent<Toggle>().isOn = true;
+            }
+        }
+    }
 
 }
