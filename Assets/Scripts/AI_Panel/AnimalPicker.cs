0ca8a06b9025053fb3b0e685e1e13dda3e3a47e4
Sabz <sabrina.d.mello@hotmail.com>
2018-04-09 15:12:53 -0400

    some stuff with GUI

--- Assets/Scripts/AI_Panel/AnimalPicker.cs
+++ Assets/Scripts/AI_Panel/AnimalPicker.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class AnimalPicker : MonoBehaviour {
 
+    bool SelectionEnabled;
+    GameObject currentChoice;
+    bool F1, F2, F3, F4;
+
+    public GameObject CowChoices;
+    public GameObject PigChoices;
+    public GameObject SheepChoices;
+    public GameObject RamChoices;
+    
     public GameObject[] MimicAnimalOptions;
     public Transform[] cows;
     public Transform[] pigs;
@@ -14,8 +23,7 @@ public class AnimalPicker : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
     {
-        
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () 
@@ -26,7 +34,7 @@ public class AnimalPicker : MonoBehaviour {
         //    setFilteredList();
         //}
 
-        MimicAnimalOptions = GameObject.FindGameObjectsWithTag("AI_Animal"); //Getting animals but we want clones
+        //MimicAnimalOptions = GameObject.FindGameObjectsWithTag("AI_Animal"); //Getting animals but we want clones
 
         HandleInput();
 
@@ -35,22 +43,45 @@ public class AnimalPicker : MonoBehaviour {
     //Display the appropriate list of animal sub-category for player to see.
     void HandleInput()
     {
-        if(Input.GetKeyDown(KeyCode.F1))
-        {
-            
+        if(Input.GetKeyDown(KeyCode.F1) && !F2 && !F3 && !F4)
+        {       
+            currentChoice = CowChoices;
+            SelectionEnabled = !SelectionEnabled;
+            F1 = !F1;       
         }
-        else if(Input.GetKeyDown(KeyCode.F2)) 
+        else if(Input.GetKeyDown(KeyCode.F2) && !F1 && !F3 && !F4) 
         {
-            
+            currentChoice = PigChoices;
+            SelectionEnabled = !SelectionEnabled;
+            F2 = !F2;
         }
-        else if(Input.GetKeyDown(KeyCode.F3)) 
+        else if(Input.GetKeyDown(KeyCode.F3) && !F1 && !F2 && !F4) 
         {
-            
+            currentChoice = SheepChoices;
+            SelectionEnabled = !SelectionEnabled;
+            F3 = !F3;
         }
-        else if(Input.GetKeyDown(KeyCode.F4)) 
+        else if(Input.GetKeyDown(KeyCode.F4) && !F1 && !F2 && !F3) 
         {
-            
+            currentChoice = RamChoices;
+            SelectionEnabled = !SelectionEnabled;
+            F4 = !F4;
         }
+
+        //if (Input.GetAxis("Mouse ScrollWheel") > 0 && SelectionEnabled)
+        //{
+        //    Debug.Log("Scrolling up");
+        //}
+
+        //if (Input.GetAxis("Mouse ScrollWheel") < 0 && SelectionEnabled)
+        //{
+        //    Debug.Log("Scrolling down");
+        //}
+
+        if (SelectionEnabled)
+            currentChoice.SetActive(true);
+        else
+            currentChoice.SetActive(false);
     }
 
     void setFilteredList() 
@@ -64,11 +95,13 @@ public class AnimalPicker : MonoBehaviour {
     //returns a filtered list of animal clones of designated (type)
     Transform[] GetFilteredAnimalType(string type) {
         //TODO
+
+        Transform[] animals;
         foreach(GameObject animal in MimicAnimalOptions)
         {
             if(animal.name.Contains(type))
             {
-                return animal.transform;
+
             }
         }
         return null;
---
+++ Assets/Scripts/AI_Panel/CreateToggleGUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateToggleGUI : MonoBehaviour
+{
+//USELESS
+
+
+    private ToggleGroup m_toggleGroup;
+
+    GameObject[] ChildrenToggles;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_toggleGroup = GetComponent<ToggleGroup>();
+        ChildrenToggles = new GameObject[transform.childCount];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChildrenToggles == null)
+            GetListOfTogglesInChildren();
+        
+    }
+
+    void GetListOfTogglesInChildren()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            ChildrenToggles[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+
+}
