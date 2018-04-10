c2f4a7bb0e2e6bad3328f82d0a285e37da2fde0e
Sabz <sabrina.d.mello@hotmail.com>
2018-04-09 21:48:22 -0400

    fixed a miss-named script

--- Assets/Scripts/AI_Panel/TransformGUI.cs
+++ Assets/Scripts/AI_Panel/TransformGUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CreateToggleGUI : MonoBehaviour
+public class TransformGUI : MonoBehaviour
 {
 
     // Use this for initialization
