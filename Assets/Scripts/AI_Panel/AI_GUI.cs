fb57db8887ede13b140851f346ab1c725d1ba3fb
Sabz <sabrina.d.mello@hotmail.com>
2018-04-11 08:45:43 -0400

    Added stats GUI (not connected yet to the AI stats)

--- Assets/Scripts/AI_Panel/AI_GUI.cs
+++ Assets/Scripts/AI_Panel/AI_GUI.cs
@@ -3,61 +3,96 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AI_GUI : MonoBehaviour {
+public class AI_GUI : MonoBehaviour
+{
 
+    //Name of Animal
     public string mString;
+    public GameObject NameStatPanel;
     public GameObject NameText;
     public Text mText;
-    public float fadeTime;
     public bool displayName;
+
+    //Stats of Animal
+    public GameObject StatsPanel;   //when set to active images and text will be active as well
+    public GameObject Stat1Text;
+    public GameObject Stat2Text;
+    public GameObject StatName;
+    public Text mStatName;
+    public Text mStat1Text;
+    public Text mStat2Text;
     public bool displayStats;
 
-	void Start () 
+    public float fadeTime;
+
+
+    void Start()
     {
+        NameStatPanel.SetActive(true);
         NameText.SetActive(true);
         displayName = true;
         mText = NameText.GetComponentInChildren<Text>();
         mText.color = Color.clear;
-	}
-	
+    }
 
-	void Update () 
+
+    void Update()
     {
         FadeText();
-	}
-
-    //void OnMouseOver()
-    //{
-    //    displayStats = true;
-    //}
-
-    //void OnMouseExit()
-    //{
-    //    displayStats = false;
-   // }
+    }
 
     void FadeText()
     {
-        /*
-        if(displayStats)
+        if (displayStats)
         {
-            mText.text = mString;
-            mText.color = Color.Lerp(mText.color, Color.white, fadeTime * Time.deltaTime);
-        }
-        else 
-        {
-            mText.color = Color.Lerp(mText.color, Color.clear, fadeTime * Time.deltaTime);
-        }
-        */
-
-        if (displayName)
-        {
-            mText.text = mString;   //Name of the Animal is always displayed
-            mText.color = Color.Lerp(mText.color, Color.black, fadeTime * Time.deltaTime);
+            StatsPanel.SetActive(true);
+            NameStatPanel.SetActive(false);
+            NameText.SetActive(false);
+            mStatName = StatName.GetComponentInChildren<Text>();
+            mStat1Text = Stat1Text.GetComponentInChildren<Text>();
+            mStat2Text = Stat2Text.GetComponentInChildren<Text>();
+            mStatName.text = mString;
+            mStat1Text.text = CurrentValueOfStat1();
+            mStat2Text.text = CurrentValueOfStat2();
+            mStatName.color = Color.Lerp(mText.color, Color.black, fadeTime * Time.deltaTime);
         }
         else
         {
-            displayName = false;
+            NameStatPanel.SetActive(true);
+            NameText.SetActive(true);
+            if (StatsPanel != null)
+                StatsPanel.SetActive(false);
+            displayName = true;
+            mText.text = mString;  
+            mText.color = Color.Lerp(mText.color, Color.black, fadeTime * Time.deltaTime);
+        }
+
+    }
+
+    private string CurrentValueOfStat1()
+    {
+        //TODO: Grab the stat1 here
+        return "FuncStat1";
+    }
+
+    private string CurrentValueOfStat2()
+    {
+        //TODO: Grab the stat1 here
+        return "FuncStat1";
+    }
+
+    //Get and Set
+    //Will be set with framer radius trigger
+    public bool DisplayName
+    {
+        get
+        {
+            return displayName;
+        }
+
+        set
+        {
+            displayName = value;
         }
     }
 }
