76de5065ec4925fe69db969c8f360bee1397b776
JJSCLOUD <jjscloud2@gmail.com>
2018-04-11 21:35:36 -0400

    added functionality for farmer and mimic to interact with world. Added ability for mimic to kill animals. TODO: need to link with kill script and AI

--- Assets/Scripts/InteractAndAttack/Shoot_Attack_Interact.cs
+++ Assets/Scripts/InteractAndAttack/Shoot_Attack_Interact.cs
@@ -4,37 +4,60 @@ using UnityEngine;
 
 public class Shoot_Attack_Interact : MonoBehaviour {
 
+    public LayerMask interactables;
+    public LayerMask layerOfCreatures;
+    RaycastHit hit;
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-
-    //TODO
-    bool IsFacingTarget()
-    {
-        return false;
-    }
-    //TODO
-    bool IsWithinDistance()
-    {
-
-        return false;
+        InteractWithWorld();
+        KillTarget();
     }
 
-    void Interact()
+    void InteractWithWorld()
     {
-        //TODO #use right click
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            Collider[] interactbleObjects = Physics.OverlapSphere(transform.position, 5f, interactables);
+            foreach(Collider interactable in interactbleObjects)
+            {
+                Vector3 direction = interactable.transform.position - transform.position;
+                //call interact script if mimic, if farmer, fill food and stuff...
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 5f, interactables))
+                {
+                    //call interact script
+                    Debug.Log("interactable Object " + hit.collider.gameObject.name);
+                }
+            }
+        }
     }
 
+    //currently implemented for mimic
     void KillTarget()
     {
-        //TODO #use left click 
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (gameObject.tag == "mimic")
+            {
+                Collider[] creatures = Physics.OverlapSphere(transform.position, 5f, layerOfCreatures);
+                foreach (Collider creature in creatures)
+                {
+                    if (Physics.Raycast(transform.position, transform.forward, out hit, 5f, layerOfCreatures))
+                    {
+                        //call kill script
+                        Debug.Log("killed " + hit.collider.gameObject.name);
+                    }
+                }
+            }
+            else if (gameObject.tag == "Player")
+            {
+                //need to shoot ray from gun
+            }
+            
+        }
     }
-
 }
